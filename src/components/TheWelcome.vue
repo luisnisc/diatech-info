31bb1a412a47bae0e5c1759bbee708f3f0a62f47
luisnisc <91005401+luisnisc@users.noreply.github.com>
2022-12-20 23:10:14 +0100

    Add files via upload

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,60 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconComponentes.vue";
+import ToolingIcon from "./icons/IconMovil.vue";
+import EcosystemIcon from "./icons/IconPeriféricos.vue";
+import CommunityIcon from "./icons/IconSilla.vue";
+import SupportIcon from "./icons/IconVisitanos.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Componentes</template>
+    Componentes y todo relacionado con procesadores
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Smartphones</template>
+
+    Ofrecemos un amplio catálogo de smartphones de diversas marcas
+
+    <br />
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Periféricos</template>
+
+    En nuestra tienda podras encontrar todo tipo de periféricos
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Sillas</template>
+
+    Encuentra el mejor confort con nuestras sillas de marcas como "SECRETLAB"
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Visitanos</template>
+
+    Ven a visitarnos a nuestra páguina web
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"
+      >Diatech.com</a
+    >.
+  </WelcomeItem>
+</template>
---
+++ src/components/icons/IconMovil.vue
@@ -0,0 +1,10 @@
+<!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
+<template>
+  <img src="./movil.png">
+</template>
+<style>
+  img{
+    width: 24px;
+    height: 24px;
+  }
+</style>
